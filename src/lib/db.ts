35b6bf3cd5e40502bc633310e84c55bc62bb7bdb
Jonathan Chiasson <jchiasson@apollocode.ca>
2026-08-23 20:08:29 -0400

    Thème

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -9,7 +9,7 @@ const globalForPrisma = globalThis as unknown as {
 
 function createPrismaClient() {
     const adapter = new PrismaPg({
-        connectionString: process.env.NEXT_PUBLIC_DATABASE_URL!,
+        connectionString: process.env.DATABASE_URL!,
     });
 
     return new PrismaClient({
